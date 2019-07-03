116a00b14e0ddaff09bd950f07d02d2f4bfb59cb
Marcin Parafiniuk <mc.parafiniuk@uw.edu.pl>
2019-07-03 13:09:12 +0200

    m

--- install_base.sh
+++ install_base.sh
@@ -2,18 +2,24 @@
 
 set -e
 
+#wget -qO - https://downloads.iridiumbrowser.de/ubuntu/iridium-release-sign-01.pub | sudo apt-key add -
+#echo "deb [arch=amd64] https://downloads.iridiumbrowser.de/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/iridium.list
 #echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
 #echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic-updates main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
 #echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic-backports main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
 #echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic-security main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
 #sudo apt-get update
 
-sudo apt-get install zsh git build-essential software-properties-common vim-gtk compton alsa-utils pulseaudio xinit software-properties-common firefox -y
-#sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get install zsh git build-essential software-properties-common vim-gtk compton alsa-utils pulseaudio xinit software-properties-common i3 iridium-browser rxvt-unicode -y
 #echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get --allow-unauthenticated install sur5r-keyring
-sudo apt-get update
-sudo apt-get install oracle-java8-installer i3 -y
+
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+
+cat <<EOT >> /etc/systemd/system/getty@tty1.service.d/override.conf
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --noissue --autologin dzastin %I $TERM
+Type=idle
+EOT
 
 #after running this script install and run configuremegently
