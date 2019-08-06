25ce8d71f32fd3d9b911ec7dd226adba25b413bf
Marcin Parafiniuk <marcin.parafiniuk@codilime.com>
2019-08-06 11:06:10 +0200

    m

--- install_base.sh
+++ install_base.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 set -e
-#wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-#wget -qO - https://downloads.iridiumbrowser.de/ubuntu/iridium-release-sign-01.pub | sudo apt-key add -
-#sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-#echo "deb [arch=amd64] https://downloads.iridiumbrowser.de/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/iridium.list
-#echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
-#echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic-updates main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
-#echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic-backports main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
-#echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic-security main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
-#sudo apt-get update
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+wget -qO - https://downloads.iridiumbrowser.de/ubuntu/iridium-release-sign-01.pub | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+echo "deb [arch=amd64] https://downloads.iridiumbrowser.de/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/iridium.list
+echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
+echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic-updates main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
+echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic-backports main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
+echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt bionic-security main restricted universe multiverse' | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
 
 sudo apt-get install zsh git build-essential software-properties-common vim-gtk compton alsa-utils pulseaudio xinit software-properties-common i3 iridium-browser rxvt-unicode code wicd-curses gitk -y
 #echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
 
-sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+#sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
 
 cat <<EOT >> /etc/systemd/system/getty@tty1.service.d/override.conf
 [Service]
 ExecStart=
-ExecStart=-/sbin/agetty --noissue --autologin dzastin %I $TERM
+ExecStart=-/sbin/agetty --noissue --autologin marcin %I $TERM
 Type=idle
 EOT
 
